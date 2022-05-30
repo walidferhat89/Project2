2d5b8dd73079bfde98a976ae24eda776cb824a7e
walid ferhat <walidferhat89@gmail.com>
2022-05-30 03:17:30 -0400

    unfinish shuffle game

---
+++ src/ShuffleGame.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class ShuffleGame {
+    public static void main(String[] args) {
+    int[] array = {1,0,1};
+        Scanner scan = new Scanner(System.in);
+        String response;
+        int position;
+        System.out.println("------------------------------welcome to shuffle game--------------------------------");
+        System.out.println("guess where the 0 is ?");
+        System.out.println("are you ready to play? y/n");
+        response = scan.nextLine();
+        if (response == "y"){
+            System.out.println("pick 1, 2 or 3!");
+            position = scan.nextInt();
+            if(position==)
+
+    }
+        else {
+            System.out.println("---------------------------------see you next time!-----------------------------------");
+        }
+    }
+}
